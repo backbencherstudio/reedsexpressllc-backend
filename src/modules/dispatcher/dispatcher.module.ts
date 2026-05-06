bca787d1cc9a8cfd1e950f736b95f52f4f9ced30
Nirob Hossain <nirobhossainalip@gmail.com>
2026-05-06 15:19:54 +0600

    driver

--- src/modules/dispatcher/dispatcher.module.ts
+++ src/modules/dispatcher/dispatcher.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CarrierModule } from './carrier/carrier.module';
 import { TruckModule } from './truck/truck.module';
 import { TralierModule } from './tralier/tralier.module';
+import { DriverModule } from './driver/driver.module';
 
 @Module({
-  imports: [CarrierModule, TruckModule, TralierModule],
+  imports: [CarrierModule, TruckModule, TralierModule, DriverModule],
   controllers: [],
   providers: [],
 })
